4d8f30dae5080c8573b8b6ef8760c1183f96da74
Julien Chaumond <chaumond@gmail.com>
2015-06-09 14:58:02 +0200

    FB credentials

--- FBFrameworkClientApp/ViewController.swift
+++ FBFrameworkClientApp/ViewController.swift
@@ -15,13 +15,14 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let vc = FrameworkViewController()
-        presentViewController(vc, animated: true) {}
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let vc = FrameworkViewController()
+        presentViewController(vc, animated: true) {}
     }
 
 
