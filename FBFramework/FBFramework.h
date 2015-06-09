07563f3d1fd32ae8c9f3e59afbba3264799a406a
Julien Chaumond <chaumond@gmail.com>
2015-06-09 12:47:57 +0200

    Create Cocoa Touch Framework target

---
+++ FBFramework/FBFramework.h
@@ -0,0 +1,19 @@
+//
+//  FBFramework.h
+//  FBFramework
+//
+//  Created by Julien Chaumond on 09/06/2015.
+//  Copyright (c) 2015 Stupeflix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FBFramework.
+FOUNDATION_EXPORT double FBFrameworkVersionNumber;
+
+//! Project version string for FBFramework.
+FOUNDATION_EXPORT const unsigned char FBFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FBFramework/PublicHeader.h>
+
+
